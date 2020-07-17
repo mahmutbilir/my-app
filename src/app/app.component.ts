674f8399eb0a415396f37c1acc749731938b4d28
Mahmut Bilir <mahmut-bilir@hotmail.com>
2020-07-18 01:57:07 +0300

    ngFor example added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  todos=[
+'Yapılacak iş 1',
+'Yapılacak iş 2',
+'Yapılacak iş 3',
+'Yapılacak iş 4',
+'Yapılacak iş 5',
+'Yapılacak iş 6',
+'Yapılacak iş 7',
+'Yapılacak iş 8',
+'Yapılacak iş 9'
+  ];
 }
