58ce3ca6361c1725f6e3eae4494e0dd96c2d4ffa
Mahmut Bilir <mahmut-bilir@hotmail.com>
2020-07-17 22:58:59 +0300

    test service created and used an example without using service for showing benefiti

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+callCenter:string;
   constructor() { }
 
   ngOnInit(): void {
+    this.callCenter='444 1 123';
   }
+
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TestService} from '../test.service'
 
 @Component({
   selector: 'app-header',
@@ -6,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-name:any;
-  constructor() { }
+
+//string
+//number
+//array<string>
+//boolean
+//any
+
+//name:any;
+callCenter:string;
+  constructor(private testService: TestService) { }
 
   ngOnInit(): void {
-    this.name='Mahmut';
+    //this.name='Mahmut';
+    this.callCenter='444 1 123';
   }
 
 }
